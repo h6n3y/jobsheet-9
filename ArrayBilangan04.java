ec3b6ccaba814736a9c8878dff12dd207f2e55e1
h6n3y <slashlemon@gmail.com>
2023-11-14 08:01:22 +0700

    Menambahkan Modifikasi 1 dan Modifikasi 2

--- ArrayBilangan04.java
+++ ArrayBilangan04.java
@@ -5,7 +5,7 @@ public class ArrayBilangan04 {
         int[] bil = new int[4];
 
         // Proses?
-        bil[0] = 5;
+        bil[0] = 0;
         bil[1] = 13;
         bil[2] = -7;
         bil[3] = 17;
@@ -14,5 +14,25 @@ public class ArrayBilangan04 {
         System.out.println(bil[1]);
         System.out.println(bil[2]);
         System.out.println(bil[3]);
+
+        // Modifikasi - 1
+
+        // Tipe Data
+        int bil[] = { 5, 13, -7, 17 };
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+
+        // Modifikasi - 2
+
+        // Tipe Data
+        int bil[] = { 5, 13, -7, 17 };
+
+        // Proses
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
